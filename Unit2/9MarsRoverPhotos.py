670b35f0d9045f14c2609bca2ac69c8acd1ed7de
Noahhasd <162621745+Noahhasd@users.noreply.github.com>
2024-04-10 13:29:39 -0400

    Update 9MarsRoverPhotos.py
    updated

--- Unit2/9MarsRoverPhotos.py
+++ Unit2/9MarsRoverPhotos.py
@@ -1,39 +1,29 @@
-
-#IMPORTS
+#request library
 import requests
 from PIL import Image
 from io import BytesIO
-
+#definition of the url
 def fetch_rover(base_url, date_to_search, api_key):
-    #concatenate a url with the base_url, your api key, and the extra parameters for the desired date
-    #This is the format your url request should be:
-    #https://api.nasa.gov/planetary/apod?api_key=YOUR_API_KEY&date=YYYY-MM-DD
     url = base_url+date_to_search+api_key
+    #show given url back and return data from it
     print(url)
     response = requests.get(url)
     data = response.json()
     return data
-
-#base_url is the beginning of the url that we will go to for our APOD
+#variable of each aspect of link
 base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="
-#below, paste your NASA API Key.
 api_key = "&api_key=sYlxxqiOHE2YgKOxQg9xtXZ6b1pFGF458seVL8Zm"
 date_to_search = "2015-06-03"
 rover_data = fetch_rover(base_url,date_to_search,api_key)
 
+#attempt to pull image
 print(rover_data)
-#if the response we get back has a 'url'...
-if 'url' in rover_data:
-    print("Title:", rover_data['title'])
-    print("Date:", rover_data['date'])
-    print("Explanation:", rover_data['explanation'])
-    print("HD URL:", rover_data.get('hdurl', "Not available"))
-    print("URL:", rover_data['url'])
-
-    # Open and display the image
-    response = requests.get(rover_data['url'])
+try:
+    photo_url = rover_data['photos'][0]['img_src']
+    response = requests.get(photo_url)
     image = Image.open(BytesIO(response.content))
     image.show()
-#otherwise, if it did not have a url...
-else:
+
+#do if it fails
+except:
     print("Error:", rover_data.get('msg', 'Unknown error'))
